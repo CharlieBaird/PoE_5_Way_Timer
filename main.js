20328d6843ac7ff1b23e6a72200ae741dcac481a
Charlie Baird <charliembaird@gmail.com>
2024-05-05 15:41:06 -0700

    Working double

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const readline = require('readline');
 
 // TRACK KILLS OF:
 // const trackKillsOf = "Cardinal Sanctus Vox";
-const trackKillsOf = "Aukuna, the Black Sekhema";
+// const trackKillsOf = "Aukuna, the Black Sekhema";
 // const trackKillsOf = "Viper Napuatzi";
 // const trackKillsOf = "Queen Hyrri Ngamaku";
 // const trackKillsOf = "General Marceus Lioneye";
 
 // RESET ON VOICELINE OF:
-const resetOnVoicelineOf = "Cardinal Sanctus Vox";
+// const resetOnVoicelineOf = "Cardinal Sanctus Vox";
 // const resetOnVoicelineOf = "Aukuna, the Black Sekhema";
 // const resetOnVoicelineOf = "Viper Napuatzi";
 // const resetOnVoicelineOf = "Queen Hyrri Ngamaku";
@@ -28,6 +28,7 @@ function createWindow() {
         width: 128,
         height: 72,
         frame: false, // Remove window frame
+
         webPreferences: {
             nodeIntegration: true
         },
@@ -44,6 +45,8 @@ function createWindow() {
     win.webContents.on('did-finish-load', () => {
         win.webContents.setAudioMuted(true);
     });
+    
+    countLinesInitial();
 }
 
 app.whenReady().then(() => {
@@ -52,7 +55,7 @@ app.whenReady().then(() => {
     // Register a 'CommandOrControl+X' shortcut listener.
     // const ret = globalShortcut.register('CommandOrControl+X', () => {
         
-    const ret = globalShortcut.register('X', () => {
+    const ret = globalShortcut.register('Ctrl+X', () => {
         try {
             resetWindow();
 
@@ -91,13 +94,18 @@ function resetWindow()
     throttle = true;
     document.getElementById('body').style.backgroundColor = '#eeeeee'
     document.getElementById('text').innerHTML = "25";
-    document.getElementById('killCounter').innerHTML = 0;
+    document.getElementById('killCounter').innerHTML = 'T: 0';
+    document.getElementById('killCounterGeneric').innerHTML = 'All: 0';
+    
+    document.getElementById('templarBody').style.backgroundColor = '#eeeeee'
+    document.getElementById('templarText').innerHTML = "25";
     `;
     win.webContents.executeJavaScript(code);
     killCounter = 0;
+    genericKillCounter = 0;
 }
 
-function startTimer()
+function startAukunaTimer()
 {
     let code = `
     new Promise((resolve, reject) => {
@@ -116,7 +124,7 @@ function startTimer()
             {
                 document.getElementById('body').style.backgroundColor = '#eeeeee'
                 document.getElementById('text').innerHTML = "25";
-                document.getElementById('killCounter').innerHTML = 0;
+                document.getElementById('killCounter').innerHTML = 'T: 0';
                 clearInterval();
                 return;
             }
@@ -149,6 +157,58 @@ function startTimer()
     Resolve();
 }
 
+function startVoxTimer()
+{
+    let code = `
+    new Promise((resolve, reject) => {
+        let counter = 24;
+        templarThreads = templarThreads + 1;
+        throttle = false;
+
+        document.getElementById('templarText').innerHTML = "25";
+        document.getElementById('templarBody').style.backgroundColor = '#eeeeee'
+
+        let changedTime = Date.now();
+
+        const intervalId = setInterval(() => {
+
+            if (throttle)
+            {
+                document.getElementById('templarBody').style.backgroundColor = '#eeeeee'
+                document.getElementById('templarText').innerHTML = "25";
+                document.getElementById('killCounter').innerHTML = 'T: 0';
+                clearInterval();
+                return;
+            }
+
+            document.getElementById('templarBody').style.backgroundColor = '#eeeeee'
+            if (counter <= 2)
+            {
+                document.getElementById('templarBody').style.backgroundColor = '#96FF62'
+            }
+            if (counter === 0 || templarThreads > 1)
+            {
+                if (templarThreads === 1) document.getElementById('templarText').innerHTML = '0';
+                clearInterval(intervalId);
+                templarThreads = templarThreads - 1;
+
+                let detectedLastLineTime = Date.now();
+                let difference = detectedLastLineTime - changedTime;
+                // console.log("Elapsed: " + difference);
+            }
+            else
+            {
+                document.getElementById('templarText').innerHTML = counter;
+                counter = counter - 1;
+            }
+        }, 1000);
+    });
+    `;
+    win.webContents.executeJavaScript(code);
+    
+    Resolve();
+}
+
 const filePath = "C:\\Program Files (x86)\\Grinding Gear Games\\Path of Exile\\logs\\Client.txt";
 
 let changedTime;
@@ -165,51 +225,126 @@ const processLatestLine = (line) => {
     let difference = detectedLastLineTime - changedTime;
     // console.log("Elapsed: " + difference);
     
-    if (line.indexOf(trackKillsOf) !== -1)
+    if (line.indexOf(MARAKETH) !== -1)
     {
         try {
-            startTimer();
+            startAukunaTimer();
 
         }
         catch (error) {}
     }
-    else if (line.indexOf("Timeless Conflict") !== -1)
+
+    if (line.indexOf(TEMPLAR) !== -1)
     {
-        killCounter = 0;
-        let code = `document.getElementById('killCounter').innerHTML = ${killCounter};`;
-        win.webContents.executeJavaScript(code);
+        try {
+            updateKillCounter();
+            startVoxTimer();
+
+
+        }
+        catch (error) {}
     }
 
-    if (line.indexOf(resetOnVoicelineOf) !== -1)
+    if (line.indexOf(TEMPLAR) !== -1 || line.indexOf(ETERNAL) !== -1 || line.indexOf(KARUI) !== -1 || line.indexOf(VAAL) !== -1 || line.indexOf(MARAKETH) !== -1)
     {
-        killCounter++;
-
-        let code = `document.getElementById('killCounter').innerHTML = ${killCounter};`;
-        win.webContents.executeJavaScript(code);
+        updateGenericKillCounter(line);
     }
-
-    // if (line.indexOf(TEMPLAR) !== -1 || line.indexOf(ETERNAL) !== -1 || line.indexOf(KARUI) !== -1 || line.indexOf(VAAL) !== -1)
-    // {
-    //     killCounter++;
-
-    //     let code = `document.getElementById('killCounter').innerHTML = ${killCounter};`;
-    //     win.webContents.executeJavaScript(code);
-    // }
-
-    // if (line.indexOf(MARAKETH) !== -1)
-    // {
-    //     killCounter++;
-
-    //     let code = `document.getElementById('killCounter').innerHTML = ${killCounter};`;
-    //     win.webContents.executeJavaScript(code);
-    // }
 };
+
+let executingArr = new Array(5).fill(false);
+let genericKillCounter = 0;
+function updateGenericKillCounter(line)
+{
+    if (line.indexOf(TEMPLAR) !== -1) {
+        if (executingArr[0]) return;
+        executingArr[0] = true;
+        genericKillCounter++;
+        let code = `document.getElementById('killCounterGeneric').innerHTML = 'All: ${genericKillCounter}';`;
+        win.webContents.executeJavaScript(code);
+        setTimeout(() => {executingArr[0] = false; }, 10000);
+    }
+    else if (line.indexOf(ETERNAL) !== -1) {
+        if (executingArr[1]) return;
+        executingArr[1] = true;
+        genericKillCounter++;
+        let code = `document.getElementById('killCounterGeneric').innerHTML = 'All: ${genericKillCounter}';`;
+        win.webContents.executeJavaScript(code);
+        setTimeout(() => {executingArr[1] = false; }, 10000);
+
+    }
+    else if (line.indexOf(KARUI) !== -1) {
+        if (executingArr[2]) return;
+        executingArr[2] = true;
+        genericKillCounter++;
+        let code = `document.getElementById('killCounterGeneric').innerHTML = 'All: ${genericKillCounter}';`;
+        win.webContents.executeJavaScript(code);
+        setTimeout(() => {executingArr[2] = false; }, 10000);
+
+    }
+    else if (line.indexOf(VAAL) !== -1) {
+        if (executingArr[3]) return;
+        executingArr[3] = true;
+        genericKillCounter++;
+        let code = `document.getElementById('killCounterGeneric').innerHTML = 'All: ${genericKillCounter}';`;
+        win.webContents.executeJavaScript(code);
+        setTimeout(() => {executingArr[3] = false; }, 10000);
+
+    }
+    else if (line.indexOf(MARAKETH) !== -1) {
+        if (executingArr[4]) return;
+        executingArr[4] = true;
+        genericKillCounter++;
+        let code = `document.getElementById('killCounterGeneric').innerHTML = 'All: ${genericKillCounter}';`;
+        win.webContents.executeJavaScript(code);
+        setTimeout(() => {executingArr[4] = false; }, 20000);
+
+    }
+}
+
+let isExecuting = false;
+function updateKillCounter()
+{
+    if (!isExecuting) {
+        // Set the flag to indicate that the method is currently being executed
+        isExecuting = true;
+    
+        // Your code block that you want to throttle
+        // For example:
+        // console.log("Executing...");
+
+        killCounter++;
+        let code = `document.getElementById('killCounter').innerHTML = 'T: ${killCounter}';`;
+        win.webContents.executeJavaScript(code);
+    
+        // Reset the flag after 10 seconds
+        setTimeout(() => {
+          isExecuting = false;
+        }, 10000); // 10 seconds in milliseconds
+      } else {
+        // Method is already executing, throttle the call
+        // console.log("Throttling...");
+        // Optionally, you can add logic here to handle the throttled call
+      }
+}
+
+let lineCounterSaved = 0;
+let lineCounter = 0;
+
+function countLinesInitial()
+{
+    // Read the file synchronously
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    
+    // Split the content by newline characters
+    const lines = fileContent.split('\n');
+    
+    // Count the number of lines
+    lineCounterSaved = lines.length-1;
+}
 
 // Listen for changes to the file
 fs.watch(filePath, (eventType, filename) => {
 if (eventType === 'change') {
-
-    changedTime = Date.now();
     
     const fileStream = fs.createReadStream(filePath);
     const rl = readline.createInterface({
@@ -217,13 +352,21 @@ if (eventType === 'change') {
         crlfDelay: Infinity
     });
 
-    let lastLine = '';
+    lineCounter = 0;
+
     rl.on('line', (line) => {
-        lastLine = line;
+        lineCounter++;
+
+        if (lineCounter > lineCounterSaved)
+        {
+            processLatestLine(line);
+        }
     });
 
     rl.on('close', () => {
-        detectedLastLineTime = Date.now();
-        processLatestLine(lastLine);
+        // detectedLastLineTime = Date.now();
+        // processLatestLine(lastLine);
+
+        lineCounterSaved = lineCounter;
     });
 }});
